2f5c0a42b7e2b1998d10e2346ca0c060e6d53bf1
김영우 <kyw05171@gmail.com>
2021-10-04 21:41:15 +0900

    04-19 키 값을 찾는 함수를 인자로 사용

---
+++ chap04 - 필터링 연산자/04-19 키 값을 찾는 함수를 인자로 사용.js
@@ -0,0 +1,16 @@
+const { of } = require("rxjs");
+const { distinct, map } = require("rxjs/operators");
+
+const obj = [
+  { id: 1, value: 20 },
+  { id: 2, value: 40 },
+  { id: 3, value: 70 },
+  { id: 1, value: 20 },
+  { id: 2, value: 40 },
+  { id: 3, value: 70 },
+];
+
+of(...obj).pipe(
+  distinct(obj => obj.id),
+  map(obj => obj.value),
+).subscribe(value => console.log(`result: ${value}`));
