175a84acea44cd4cf6a3fb7d0fe66ef80d2a904a
김영우 <kyw05171@gmail.com>
2021-10-09 18:25:49 +0900

    05-27 durationSelector 함수를 사용하는 예

---
+++ chap05 - 변환 연산자/05-27 durationSelector 함수를 사용하는 예.js
@@ -0,0 +1,27 @@
+const { interval } = require("rxjs");
+const { groupBy, take, mergeMap, map, reduce, tap } = require("rxjs/operators");
+
+interval(500).pipe(
+  take(10),
+  groupBy(
+    () => Math.random() < 0.7,
+    value => `${value}-${value % 2 === 0 ? "짝수" : "홀수"}`,
+    groupedObservable => groupedObservable.key === true
+      ? interval(600).pipe(
+        tap(value => console.log(`당첨 duration ${value}`)),
+      )
+      : interval(2000).pipe(
+        tap(value => console.log(`꽝 duration ${value}`)),
+      ),
+  ),
+  mergeMap(groupedObservable => groupedObservable.key === true
+    ? groupedObservable.pipe(
+      map(value => `당첨!! ${value}`),
+      reduce((acc, curr) => [...acc, curr], []),
+    )
+    : groupedObservable.pipe(
+      map(value => `꽝!! ${value}`),
+      reduce((acc, curr) => [...acc, curr], []),
+    ),
+  ),
+).subscribe(result => console.log(result));
