cdea523c9215e44b3cadb04b058673683a1fddd4
김영우 <kyw05171@gmail.com>
2021-10-14 00:36:01 +0900

    11-11 서브젝트의 unsubscribe 함수의 동작

---
+++ chap11 - 서브젝트/11-11 서브젝트의 unsubscribe 함수의 동작.js
@@ -0,0 +1,33 @@
+const { Subject } = require("rxjs");
+
+const observerA = {
+  next: result => console.log(`observerA: ${result}`),
+  error: error => console.log(`observerA ERROR: ${error}`),
+  complete: () => console.log(`observerA: complete`),
+};
+
+const observerB = {
+  next: result => console.log(`observerB: ${result}`),
+  error: error => console.log(`observerB ERROR: ${error}`),
+  complete: () => console.log(`observerB: complete`),
+};
+
+const observerC = {
+  next: result => console.log(`observerC: ${result}`),
+  error: error => console.log(`observerC ERROR: ${error}`),
+  complete: () => console.log(`observerC: complete`),
+};
+
+const mySubject = new Subject();
+mySubject.subscribe(observerA);
+mySubject.subscribe(observerB);
+mySubject.subscribe(observerC);
+
+mySubject.unsubscribe();
+
+// ObjectUnsubscribedError 발생
+
+// mySubject.subscribe(observerA);
+// mySubject.next(3);
+// mySubject.error(new Error("Hello World"));
+// mySubject.complete();
