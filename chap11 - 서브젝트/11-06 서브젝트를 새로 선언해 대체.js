b2101b50ee1e33a10617162d85aecbf9e4b6bed3
김영우 <kyw05171@gmail.com>
2021-10-14 00:03:21 +0900

    11-06 서브젝트를 새로 선언해 대체

---
+++ chap11 - 서브젝트/11-06 서브젝트를 새로 선언해 대체.js
@@ -0,0 +1,26 @@
+const { Subject, interval } = require("rxjs");
+const { take } = require("rxjs/operators");
+
+const mySubject = new Subject();
+
+const intervalSource$ = interval(500).pipe(
+  take(5),
+);
+
+const observerA = {
+  next: result => console.log(`observerA: ${result}`),
+  error: error => console.log(`observerA ERROR: ${error}`),
+  complete: () => console.log(`observerA: complete`),
+};
+
+const observerB = {
+  next: result => console.log(`observerB: ${result}`),
+  error: error => console.log(`observerB ERROR: ${error}`),
+  complete: () => console.log(`observerB: complete`),
+};
+
+mySubject.subscribe(observerA);
+
+setTimeout(() => {
+  mySubject.subscribe(observerB);
+}, 2000);
