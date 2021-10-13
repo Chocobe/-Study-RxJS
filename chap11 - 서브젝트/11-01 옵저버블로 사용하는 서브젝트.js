fc2a984d3166712f15f3df6bcacd3a2b954146e6
김영우 <kyw05171@gmail.com>
2021-10-13 23:20:43 +0900

    11-01 옵저버블로 사용하는 서브젝트

---
+++ chap11 - 서브젝트/11-01 옵저버블로 사용하는 서브젝트.js
@@ -0,0 +1,25 @@
+const { Subject } = require("rxjs");
+
+const mySubject = new Subject();
+
+const observerA = {
+  next: result => console.log(`observerA: ${result}`),
+  error: error => console.log(`observerA: ${error}`),
+  complete: () => console.log(`observerA: complete`),
+};
+
+const observerB = {
+  next: result => console.log(`observerB: ${result}`),
+  error: error => console.log(`observerB: ${error}`),
+  complete: () => console.log(`observerB: complete`),
+};
+
+const observerC = {
+  next: result => console.log(`observerC: ${result}`),
+  error: error => console.log(`observerC: ${error}`),
+  complete: () => console.log(`observerC: complete`),
+};
+
+mySubject.subscribe(observerA);
+mySubject.subscribe(observerB);
+mySubject.subscribe(observerC);
