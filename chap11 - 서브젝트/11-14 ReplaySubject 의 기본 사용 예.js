0e34d72111796ae5dd905458eff977fc68330927
김영우 <kyw05171@gmail.com>
2021-10-15 01:02:32 +0900

    11-14 ReplaySubject 의 기본 사용 예

---
+++ chap11 - 서브젝트/11-14 ReplaySubject 의 기본 사용 예.js
@@ -0,0 +1,28 @@
+const { ReplaySubject, interval } = require("rxjs");
+const { take } = require("rxjs/operators");
+
+const intervalSource$ = interval(500).pipe(
+  take(8),
+);
+
+const observerA = {
+  next: result => console.log(`observerA: ${result}`),
+  error: error => console.log(`observerA ERROR: ${error}`),
+  complete: () => console.log(`observerA: complete`),
+};
+
+const observerB = {
+  next: result => console.log(`observerB: ${result}`),
+  error: error => console.log(`observerB ERROR: ${error}`),
+  complete: () => console.log(`observerB: complete`),
+};
+
+const replaySubject = new ReplaySubject(3);
+
+console.log("replaySubject 구독: 옵저버A");
+replaySubject.subscribe(observerA);
+
+console.log("intervalSource$ 구독: replaySubject");
+intervalSource$.subscribe(replaySubject);
+
+setTimeout(() => replaySubject.subscribe(observerB), 2600);
